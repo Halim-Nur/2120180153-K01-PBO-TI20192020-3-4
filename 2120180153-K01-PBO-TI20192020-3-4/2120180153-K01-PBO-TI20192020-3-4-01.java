365fe5fca53a0915f093f73d82bf4ec4b4162391
BagasPrimada <55829746+BagasPrimada@users.noreply.github.com>
2019-10-31 00:15:21 +0700

    Upload tugas 01

---
+++ 2120180153-K01-PBO-TI20192020-3-4/2120180153-K01-PBO-TI20192020-3-4-01.java
@@ -0,0 +1,13 @@
+package mahasiswa;
+public class mahasiswa {
+    String alamat;
+    String nama;
+    String nim;
+    
+    void biodata(){
+        System.out.println("Biodata Mahasiswa:");
+        System.out.println("Alamat:" + alamat);
+        System.out.println("Nama:" + nama);
+        System.out.println("NIM :" + nim);
+    }
+}
